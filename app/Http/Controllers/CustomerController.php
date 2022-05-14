c859d5eec31eb03a1d86c885fd5e7d059923482a
sasanurjaman <nurjamansasa97@gmail.com>
2022-05-14 19:13:46 +0700

    autorization customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
+    // autorization
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
