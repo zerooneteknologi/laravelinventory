3fab057d1077e55f4138259fc301de31c28cdcde
sasanurjaman <nurjamansasa97@gmail.com>
2022-07-30 23:10:46 +0700

    notifications stock product & credit

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -23,8 +23,8 @@
             </li>
             <li class="nav-item">
                 <div class="dropdown">
-                    <a class="dropdown-toggle h-drop" href="#" data-toggle="dropdown">
-                        Dropdown
+                    <a class="dropdown-toggle h-drop m-l-10 text-uppercase" href="#" data-toggle="dropdown">
+                        Welcome Back {{ auth()->user()->name}}
                     </a>
                     <div class="dropdown-menu profile-notification ">
                         <ul class="pro-body">
@@ -35,121 +35,58 @@
                     </div>
                 </div>
             </li>
-            <li class="nav-item">
-                <div class="dropdown mega-menu">
-                    <a class="dropdown-toggle h-drop" href="#" data-toggle="dropdown">
-                        Mega
-                    </a>
-                    <div class="dropdown-menu profile-notification ">
-                        <div class="row no-gutters">
-                            <div class="col">
-                                <h6 class="mega-title">UI Element</h6>
-                                <ul class="pro-body">
-                                    <li><a href="#!" class="dropdown-item"><i class="fas fa-circle"></i> Alert</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="fas fa-circle"></i> Button</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="fas fa-circle"></i> Badges</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="fas fa-circle"></i> Cards</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="fas fa-circle"></i> Modal</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="fas fa-circle"></i> Tabs & pills</a></li>
-                                </ul>
-                            </div>
-                            <div class="col">
-                                <h6 class="mega-title">Forms</h6>
-                                <ul class="pro-body">
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-minus"></i> Elements</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-minus"></i> Validation</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-minus"></i> Masking</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-minus"></i> Wizard</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-minus"></i> Picker</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-minus"></i> Select</a></li>
-                                </ul>
-                            </div>
-                            <div class="col">
-                                <h6 class="mega-title">Application</h6>
-                                <ul class="pro-body">
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-mail"></i> Email</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-clipboard"></i> Task</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-check-square"></i> To-Do</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-image"></i> Gallery</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-help-circle"></i> Helpdesk</a></li>
-                                </ul>
-                            </div>
-                            <div class="col">
-                                <h6 class="mega-title">Extension</h6>
-                                <ul class="pro-body">
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-file-plus"></i> Editor</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-file-minus"></i> Invoice</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-calendar"></i> Full calendar</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-upload-cloud"></i> File upload</a></li>
-                                    <li><a href="#!" class="dropdown-item"><i class="feather icon-scissors"></i> Image cropper</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </li>
         </ul>
+
+        <?php  
+            $stockProduct = \DB::select("SELECT * FROM products WHERE stock <= 20");
+            $date = date('Y-m-d', strtotime("-7 days"));
+            $credits = \DB::select("SELECT * FROM credits WHERE expired = $date");
+            $count = count($stockProduct) + count($credits);
+        ?>
         <ul class="navbar-nav ml-auto">
             <li>
                 <div class="dropdown">
                     <a class="dropdown-toggle" href="#" data-toggle="dropdown">
                         <i class="icon feather icon-bell"></i>
-                        <span class="badge badge-pill badge-danger">5</span>
+                        <span class="badge badge-pill badge-danger">{{ $count }}</span>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right notification">
                         <div class="noti-head">
                             <h6 class="d-inline-block m-b-0">Notifications</h6>
-                            <div class="float-right">
-                                <a href="#!" class="m-r-10">mark as read</a>
-                                <a href="#!">clear all</a>
-                            </div>
                         </div>
                         <ul class="noti-body">
                             <li class="n-title">
-                                <p class="m-b-0">NEW</p>
+                                <p class="m-b-0">Stok Produk</p>
                             </li>
-                            <li class="notification">
-                                <div class="media">
-                                    <img class="img-radius" src="{{ asset('assets/images/user/avatar-1.jpg')}}" alt="Generic placeholder image">
-                                    <div class="media-body">
-                                        <p><strong>John Doe</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>5 min</span></p>
-                                        <p>New ticket Added</p>
+                            @foreach ($stockProduct as $item)
+                                <li class="notification">
+                                    <div class="media">
+                                        <a href="/product/{{ $item->id }}">
+                                            <h5>{{ $item->productName}}</h5>
+                                        </a>
+                                        <div class="media-body">
+                                            <p> stok sisa {{ $item->stock}}</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
+                                </li>
+                            @endforeach
                             <li class="n-title">
-                                <p class="m-b-0">EARLIER</p>
+                                <p class="m-b-0">utang jatuh Tempo</p>
                             </li>
-                            <li class="notification">
-                                <div class="media">
-                                    <img class="img-radius" src="assets/images/user/avatar-2.jpg" alt="Generic placeholder image">
-                                    <div class="media-body">
-                                        <p><strong>Joseph William</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>10 min</span></p>
-                                        <p>Prchace New Theme and make payment</p>
+                            @foreach ($credits as $credit)
+                                <li class="notification">
+                                    <div class="media">
+                                        <a href="/product/{{ $credit->id }}">
+                                            <h5>{{ $credit->dbt}}</h5>
+                                        </a>
+                                        <div class="media-body">
+                                            <p> Jatuh Tempo {{ $credit->expired}}</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                            <li class="notification">
-                                <div class="media">
-                                    <img class="img-radius" src="assets/images/user/avatar-1.jpg" alt="Generic placeholder image">
-                                    <div class="media-body">
-                                        <p><strong>Sara Soudein</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>12 min</span></p>
-                                        <p>currently login</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="notification">
-                                <div class="media">
-                                    <img class="img-radius" src="assets/images/user/avatar-2.jpg" alt="Generic placeholder image">
-                                    <div class="media-body">
-                                        <p><strong>Joseph William</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>30 min</span></p>
-                                        <p>Prchace New Theme and make payment</p>
-                                    </div>
-                                </div>
-                            </li>
+                                </li>
+                            @endforeach
                         </ul>
                         <div class="noti-footer">
-                            <a href="#!">show all</a>
                         </div>
                     </div>
                 </div>
@@ -172,11 +109,6 @@
                                 @csrf
                             </form>
                         </div>
-                        <ul class="pro-body">
-                            <li><a href="user-profile.html" class="dropdown-item"><i class="feather icon-user"></i> Profile</a></li>
-                            <li><a href="email_inbox.html" class="dropdown-item"><i class="feather icon-mail"></i> My Messages</a></li>
-                            <li><a href="auth-signin.html" class="dropdown-item"><i class="feather icon-lock"></i> Lock Screen</a></li>
-                        </ul>
                     </div>
                 </div>
             </li>
